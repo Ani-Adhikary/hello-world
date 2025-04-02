946a231961f28c9489f50a1e150bfd3464aae04f
Ani Adhikary <33363003+Ani-Adhikary@users.noreply.github.com>
2025-04-03 01:02:26 +0530

    Update Code.swift

--- Code.swift
+++ Code.swift
@@ -2,6 +2,7 @@ extension CardsCarouselView {
     private func createDragGesture(_ viewModel: CardsCarouselViewModel) -> some Gesture {
         DragGesture()
             .onChanged {
+                // Allow ONLY horizontal swipes
                 if abs($0.predictedEndTranslation.width) > abs($0.predictedEndTranslation.height) {
                     displacement = $0.translation.width
                     isDragging = true
@@ -20,3 +21,29 @@ extension CardsCarouselView {
         DragGesture()
     }
 }
+
+
+extension CardsCarouselView {
+    private func createDragGesture(_ viewModel: CardsCarouselViewModel) -> some Gesture {
+        DragGesture()
+            .onChanged {
+                // Allow ONLY vertical swipes
+                if abs($0.predictedEndTranslation.height) > abs($0.predictedEndTranslation.width) {
+                    displacement = $0.translation.height
+                    isDragging = true
+                    swipeDirection = displacement > 0 ? .down : .up
+                    calculateInformationVisibility()
+                }
+            }
+            .onEnded {
+                if abs($0.predictedEndTranslation.height) > abs($0.predictedEndTranslation.width) {
+                    userInteractionEnded(viewModel: viewModel, width: $0.translation.height)
+                }
+            }
+    }
+
+    private func emptyDragGesture() -> some Gesture {
+        DragGesture()
+    }
+}
+
