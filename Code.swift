9a8fc4c7cf5c5f67e79dab3f7fc58a2546bbf95c
Ani Adhikary <33363003+Ani-Adhikary@users.noreply.github.com>
2025-04-03 00:53:44 +0530

    Create Code.swift

---
+++ Code.swift
@@ -0,0 +1,22 @@
+extension CardsCarouselView {
+    private func createDragGesture(_ viewModel: CardsCarouselViewModel) -> some Gesture {
+        DragGesture()
+            .onChanged {
+                if abs($0.predictedEndTranslation.width) > abs($0.predictedEndTranslation.height) {
+                    displacement = $0.translation.width
+                    isDragging = true
+                    swipeDirection = displacement > 0 ? .right : .left
+                    calculateInformationVisibility()
+                }
+            }
+            .onEnded {
+                if abs($0.predictedEndTranslation.width) > abs($0.predictedEndTranslation.height) {
+                    userInteractionEnded(viewModel: viewModel, width: $0.translation.width)
+                }
+            }
+    }
+
+    private func emptyDragGesture() -> some Gesture {
+        DragGesture()
+    }
+}
